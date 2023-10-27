49a5f0025bd31200d445afb7dd279c023ee6819d
hota1024 <hotalog@hota1024.com>
2023-10-27 22:24:02 +0900

    💄add contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,36 @@
+import { Breadcrumbs } from "@/components/Breadcrumbs";
+
+import classes from "./page.module.css";
+
+export default function Contact() {
+  async function Send() {
+    "use server";
+
+    console.log("OK");
+  }
+
+  return (
+    <div className={classes.container}>
+      <Breadcrumbs
+        items={[
+          { label: "Home", href: "/" },
+          { label: "Contact", href: "/contact" },
+        ]}
+      />
+
+      <section className={classes.section}>
+        <h1>Contact</h1>
+        <p>
+          Please DM to <a href="https://x.com/hota1024">Twitter(X)</a>
+        </p>
+      </section>
+
+      <Breadcrumbs
+        items={[
+          { label: "Home", href: "/" },
+          { label: "Contact", href: "/contact" },
+        ]}
+      />
+    </div>
+  );
+}
